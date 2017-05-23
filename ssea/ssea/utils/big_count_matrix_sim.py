d62b790f9870026c9ed5161de774cad1d7cbba99
mkiyer <matthew.iyer@gmail.com>
2017-05-23 04:42:57 -0400

    new simulation script.. tested, working
    pure read simulator

--- ssea/ssea/utils/big_count_matrix_sim.py
+++ ssea/ssea/utils/big_count_matrix_sim.py
@@ -20,22 +20,26 @@ __email__ = "yniknafs@umich.edu"
 __status__ = "Development"
 
 
+ABUNDANCE_MEMMAP_FILE = 'abundances.memmap'
+COUNTS_MEMMAP_FILE = 'counts.memmap'
+
+
 class SimGene:
-    def __init__(self, name, length, fpkm, fold_change, frac_de):
+    def __init__(self, name, length, expr, fold_change, frac_de):
         self.name = name
         self.length = int(length)
-        self.fpkm = float(fpkm)
+        self.expr = float(expr)
         self.fold_change = float(fold_change)
         self.frac_de = float(frac_de)
 
     def __str__(self):
-        return '{}_{}bp_{}fpkm_{}fc_{}de'.\
-            format(self.name, self.length, self.fpkm, self.fold_change,
+        return '{}_{}bp_{}expr_{}fc_{}de'.\
+            format(self.name, self.length, self.expr, self.fold_change,
                    self.frac_de)
 
     @staticmethod
     def header():
-        return '\t'.join(['name', 'length', 'fpkm', 'fold_change', 'frac_de'])
+        return '\t'.join(['name', 'length', 'expr', 'fold_change', 'frac_de'])
 
 
 class SimSample:
@@ -52,7 +56,48 @@ class SimSample:
         return '\t'.join(['name', 'total_counts', 'membership'])
 
 
-def sim_gene_counts(g, membership, total_counts):
+def gen_samples(num_samples, num_cancers, total_count_range):
+    samples = []
+    num_normals = num_samples - num_cancers
+    for n in xrange(num_cancers):
+        total_counts = np.random.randint(total_count_range[0], total_count_range[1])
+        s = SimSample('C{}'.format(n), total_counts, 1)
+        samples.append(s)
+    for n in xrange(num_normals):
+        total_counts = np.random.randint(total_count_range[0], total_count_range[1])
+        s = SimSample('N{}'.format(n), total_counts, 0)
+        samples.append(s)
+    return samples
+
+
+def read_genes(genes_file, null_expr_frac):
+    genes = []
+    total_expr = 0.0
+    with open(genes_file, 'r') as f:
+        f.next()
+        for line in f:
+            fields = line.strip().split('\t')
+            g = SimGene(*fields)
+            genes.append(g)
+            total_expr += g.expr
+    # recalculate gene relative abundances
+    for g in genes:
+        g.expr = (g.expr / total_expr) * (1.0 - null_expr_frac)
+    return genes
+
+
+def gen_null_genes(num_null_genes, null_expr_frac, length_range):
+    null_exprs = np.random.random(num_null_genes)
+    null_exprs = (null_exprs / null_exprs.sum()) * null_expr_frac
+    genes = []
+    for i in xrange(num_null_genes):
+        length = np.random.randint(length_range[0], length_range[1])
+        g = SimGene('null{}'.format(i), length, null_exprs[i], 1.0, 0.0)
+        genes.append(g)
+    return genes
+
+
+def sim_rel_abundances(g, membership):
     # choose de at random
     hit_inds = (membership > 0).nonzero()[0]
     np.random.shuffle(hit_inds)
@@ -61,63 +106,119 @@ def sim_gene_counts(g, membership, total_counts):
     de = np.zeros(membership.shape[0], dtype=np.int)
     de[de_inds] = 1
     # pick from base or de counts
-    counts_base = (total_counts * 1.0e-6) * (g.length * 1.0e-3) * g.fpkm
-    counts_de = counts_base * g.fold_change
-    counts = np.select([de == 0, de == 1], [counts_base, counts_de])
-    # sample from poisson distribution
-    counts = np.random.poisson(counts)
+    expr_base = (g.length * 1.0e-3) * g.expr
+    expr_de = expr_base * g.fold_change
+    exprs = np.select([de == 0, de == 1], [expr_base, expr_de])
+    return exprs
+
+
+def create_relative_abundance_matrix(samples, genes, output_dir):
+    abundance_memmap_file = os.path.join(output_dir, ABUNDANCE_MEMMAP_FILE)
+    m = np.memmap(abundance_memmap_file, dtype=np.float, mode='w+',
+                  shape=(len(genes), len(samples)))
+    membership = np.array([s.membership for s in samples], dtype=np.int)
+    # apply fold change and differential expression frac to genes
+    for i, g in enumerate(genes):
+        exprs = sim_rel_abundances(g, membership)
+        m[i,:] = exprs
+    # renormalize each sample such that sample exprs sum to 1
+    for j, s in enumerate(samples):
+        total_exprs = m[:,j].sum()
+        m[:,j] /= total_exprs
+    return m
+
+
+def sim_sample_counts(relexprs, total_counts, step=100000):
+    counts = np.zeros(relexprs.shape[0], dtype=np.float)
+    probs = relexprs.cumsum()
+    c = 0
+    while (c + step) <= total_counts:
+        counts[:] += np.bincount(probs.searchsorted(np.random.random(step)), minlength=counts.shape[0])
+        c += step
+    if (total_counts - c) > 0:
+        step = total_counts - c
+        counts[:] += np.bincount(probs.searchsorted(np.random.random(step)), minlength=counts.shape[0])
     return counts
 
 
-def sim_counts(genes, samples):
-    membership = np.array([s.membership for s in samples], dtype=np.int)
+def sim_counts(abundance_mat, samples, output_dir):
     total_counts = np.array([s.total_counts for s in samples], dtype=np.int)
-    for g in genes:
-        counts = sim_gene_counts(g, membership, total_counts)
-        yield g, counts
-
-
-def gen_samples(num_samples, num_hits, total_count_range):
-    assert(num_samples >= num_hits)
-    num_misses = num_samples - num_hits
-    samples = []
-    for i in xrange(num_samples):
-        name = 's{}'.format(i)
-        total_counts = np.random.randint(*total_count_range)
-        membership = 0 if (i < num_misses) else 1
-        samples.append(SimSample(name, total_counts, membership))
-    return samples
+    counts_memmap_file = os.path.join(output_dir, COUNTS_MEMMAP_FILE)
+    count_mat = np.memmap(counts_memmap_file, dtype=np.float, mode='w+',
+                          shape=abundance_mat.shape)
+    for j, s in enumerate(samples):
+        logging.debug('Simulating reads for sample %d (%s)' % (j, str(s)))
+        counts = sim_sample_counts(abundance_mat[:, j], s.total_counts)
+        count_mat[:, j] = counts
+    return count_mat
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--num-samples', type=int, default=None)
-    parser.add_argument('--num-hits', type=int, default=None)
-    parser.add_argument('--total-count-range', type=str, default=None,
+    parser.add_argument('-v', '--verbose', dest='verbose',
+                        action="store_true",
+                        default=False,
+                        help='Enabled detailed logging '
+                        '(for debugging)')
+    parser.add_argument('-o', dest='output_dir')
+    parser.add_argument('-n', '--num-samples', dest='num_samples',
+                        type=int, default=0)
+    parser.add_argument('-ncancer', '--num-cancers', dest='num_cancers',
+                        type=int, default=0)
+    parser.add_argument('--total-count-range',
+                        type=str,
+                        default='50000000,100000000',
                         help='X,Y (e.g. 50000000,100000000)')
     parser.add_argument('--samples', dest='samples_file')
-    parser.add_argument('-o', dest='output_dir')
+    parser.add_argument('--num-null-genes', dest='num_null_genes',
+                        type=int, default=1000)
+    parser.add_argument('--null-expr-frac', dest='null_expr_frac',
+                        type=float, default=0.9)
+    parser.add_argument('--null-length-range', dest='null_length_range',
+                        type=str,
+                        default='250,25000',
+                        help='X,Y (e.g. 250,25000)')
     parser.add_argument('genes_file')
     args = parser.parse_args()
 
-    # read samples
+    # setup logging
+    if args.verbose:
+        level = logging.DEBUG
+    else:
+        level = logging.INFO
+    logging.basicConfig(level=level,
+                        format="%(asctime)s pid=%(process)d "
+                               "%(levelname)s - %(message)s")
+
+    # input args
+    num_samples = args.num_samples
+    num_cancers = args.num_cancers
+    assert num_cancers <= num_samples
+    num_normals = num_samples - num_cancers
+    total_count_range = map(int, args.total_count_range.split(','))
+    num_null_genes = args.num_null_genes
+    null_expr_frac = args.null_expr_frac
+    null_length_range = map(int, args.null_length_range.split(','))
+
+    # samples
     samples = []
+    samples.extend(gen_samples(num_samples, num_cancers, total_count_range))
+    # samples specified in file
     if args.samples_file:
         with open(args.samples_file, 'r') as f:
             f.next()
             for line in f:
                 fields = line.strip().split('\t')
                 samples.append(SimSample(*fields))
-    
 
     # read genes
-    genes = []
-    with open(args.genes_file, 'r') as f:
-        f.next()
-        for line in f:
-            fields = line.strip().split('\t')
-            genes.append(SimGene(*fields))
+    genes = read_genes(args.genes_file, null_expr_frac)
+    # generate null genes
+    genes.extend(gen_null_genes(num_null_genes, null_expr_frac,
+                                null_length_range))
 
+    abundance_mat = create_relative_abundance_matrix(samples, genes, args.output_dir)
+    count_mat = sim_counts(abundance_mat, samples, args.output_dir)
 
     # output dir
     if not os.path.exists(args.output_dir):
@@ -130,9 +231,10 @@ def main():
         for s in samples:
             fields.append(str(s))
         print >>f, '\t'.join(fields)
-        for g, counts in sim_counts(genes, samples):
+        for i in xrange(count_mat.shape[0]):
+            g = genes[i]
             fields = [str(g), str(g.length)]
-            fields.extend(map(str, counts))
+            fields.extend(map(str, count_mat[i,:]))
             print >>f, '\t'.join(fields)
 
     # print sample set file
